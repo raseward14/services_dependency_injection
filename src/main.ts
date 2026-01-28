37d3c1051cb0224c2a00527ce6b45a679e5fe3c2
raseward14 <raseward14@gmail.com>
2026-01-28 16:58:30 -0700

    created TaskStatusOptions const exported from models and imported into main.ts for custom injection token creation across the app. to use in the task filter dropdown and task status dropdown

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { InjectionToken } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
 import { TasksService } from './app/tasks/tasks.service';
+import { TaskStatusOptions } from './app/tasks/task.model';
 
 export const TasksServiceToken = new InjectionToken<TasksService>('tasks-service-token');
+export const TasksValueToken = new InjectionToken('task-status-options');
 
 bootstrapApplication(AppComponent, {
-    providers: [{provide: TasksServiceToken, useClass: TasksService}]
+    providers: [
+        {provide: TasksServiceToken, useClass: TasksService},
+        {provide: TasksValueToken, useValue: TaskStatusOptions}
+    ]
 }).catch((err) => console.error(err));
 
 // bootstrapApplication(AppComponent).catch((err) => console.error(err));
